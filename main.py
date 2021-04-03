1e62e4d8877830007862ea84bbc19d24b100195b
vpplag <74141971+vpplag@users.noreply.github.com>
2021-04-03 23:14:25 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+for index in range(5):
+    basic.show_number(index + 3)
+basic.clear_screen()
---
+++ main.ts
@@ -0,0 +1,4 @@
+for (let index = 0; index <= 4; index++) {
+    basic.showNumber(index + 1)
+}
+basic.clearScreen()
